e57b468ba3c0d7f8a31908192fec7bd6b314495e
dima91020 <vereskodima76@gmail.com>
2025-01-15 22:42:07 +0200

    update static_root

--- schedule_3_course_backend/settings.py
+++ schedule_3_course_backend/settings.py
@@ -136,7 +136,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.ManifestStaticFilesStorage'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join('schedule_3_course_backend', 'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
