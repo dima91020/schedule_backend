9cb2bef2539b325960b761d71f6dcc2cc15c8519
dima91020 <vereskodima76@gmail.com>
2025-01-16 04:38:27 +0200

    add debug settings for urls

--- schedule_3_course_backend/urls.py
+++ schedule_3_course_backend/urls.py
@@ -18,6 +18,8 @@ from django.contrib import admin
 from django.urls import path, include
 from rest_framework import routers
 from lesson import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 router = routers.DefaultRouter()
 router.register(r'lessons', views.LessonView, 'lesson')
@@ -26,3 +28,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include(router.urls)),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
